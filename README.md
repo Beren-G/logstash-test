30919503b9685d725b462319a6e15e4a4f954c47
Beren G <berengamble@gmail.com>
2019-04-29 17:32:58 +0100

    Tidy up superflous files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # logstash-test
+This makes it simple to test filebeat configuration and logstash pipelines.
--- update_json.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import os
-import subprocess
-
-path = subprocess.check_output(['pwd'])
-print(path.splitlines()[0].decode(utf-8))
-
